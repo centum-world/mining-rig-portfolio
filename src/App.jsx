491663eb8d5fa2b6c948e80360c18c757d8781c3
centum-world <centumworldminingrig@gmail.com>
2024-01-20 13:04:17 +0530

    carasoule with card center

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import Footer from "./common/Footer";
 import WhitePaper from "./pages/WhitePaper";
 import { useSelector } from "react-redux";
 
+
 const App = () => {
   const { selectedColor } = useSelector((state) => state.colors);
   const [isHeaderVisible] = useState(true);
