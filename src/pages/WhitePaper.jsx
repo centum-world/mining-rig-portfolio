2a2b83eea5047da93fdc8ebf94b5b4990cc6a989
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-01-22 13:52:11 +0530

    all done

--- src/pages/WhitePaper.jsx
+++ src/pages/WhitePaper.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import WhitepaperSection from '../components/WhitepaperSection'
-import WhitePaperLowerSection from '../components/WhitePaperLowerSection'
+import React from "react";
+import WhitepaperSection from "../components/WhitepaperSection";
+import WhitePaperLowerSection from "../components/WhitePaperLowerSection";
+import { useSelector } from "react-redux";
+import { selectDarkMode } from "../redux/darkModeSlice";
 
 const WhitePaper = () => {
+  const darkModeEnabled = useSelector(selectDarkMode);
   return (
-    <>
-        <WhitepaperSection/>
-        <WhitePaperLowerSection/>
-    </>
-  )
-}
+    <div className={` ${
+      darkModeEnabled ? "bg-gray-900" : "bg-gray-50"
+    }`}>
+      <WhitepaperSection />
+      <WhitePaperLowerSection />
+    </div>
+  );
+};
 
-export default WhitePaper
+export default WhitePaper;
